bcdc9deee5f5ee140e8e4b964bb596ba1b7ea014
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2023-08-23 17:41:24 +0200

    feat: add billboard

---
+++ actions/GetBillboard.tsx
@@ -0,0 +1,10 @@
+import type { Billboard } from '@/types';
+
+const URL = `${process.env.NEXT_PUBLIC_API_URL as string}/billboards`;
+
+const GetBillboard = async (id: string): Promise<Billboard> => {
+  const res = await fetch(`${URL}/${id}`);
+  return await res.json();
+};
+
+export default GetBillboard;
--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,3 +1,16 @@
-export default function Home() {
-  return <main className="">Hello Store</main>;
+import GetBillboard from '@/actions/GetBillboard';
+import Billboard from '@/components/Billboard';
+import Container from '@/components/ui/Container';
+
+export const revalidate = 0;
+
+export default async function Home() {
+  const billboard = await GetBillboard('9210bc12-138c-4f40-a986-19a385cd641d');
+  return (
+    <Container>
+      <main className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </main>
+    </Container>
+  );
 }
